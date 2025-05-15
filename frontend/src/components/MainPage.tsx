11bafd9854a9e529ae2f50f81f1aaa5ffcaad3fa
notlim_dev <elmotigre6@gmail.com>
2025-05-15 13:40:44 -0500

    modify compoenet root proccesing images

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -1,11 +1,9 @@
-import { Navbar } from "./elements/Navbar"
 import { Options } from "./elements/Options"
 import { SelectImage } from "./elements/SelectImage"
 
 export default function MainPage() {
   return (
-    <div className="flex flex-col h-full w-full">
-      <Navbar />
+    <div className="flex flex-col h-screen w-full">
       <main className="flex-1 grid grid-cols-6 max-md:grid-cols-1">
         <Options />
         <SelectImage />
